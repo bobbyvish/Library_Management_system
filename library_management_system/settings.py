8a695acaf6f70843f91167436821b16accba441d
bobbyvish <bobby.vishwakarma@galentictech.com>
2022-08-23 04:32:34 +0530

    first commit

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG",True)
 
-ALLOWED_HOSTS = ['library--managementsystem.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 
